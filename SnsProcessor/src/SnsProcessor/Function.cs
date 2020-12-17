511f152a300328106f21599fc3659555ead7843d
Eric Crook <eric.crook@gmail.com>
2020-12-17 12:03:32 -0600

    Initial commit

---
+++ SnsProcessor/src/SnsProcessor/Function.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SnsProcessor
+{
+    public class Function
+    {
+        /// <summary>
+        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+        /// region the Lambda function is executed in.
+        /// </summary>
+        public Function()
+        {
+        }
+
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an SNS event object and can be used 
+        /// to respond to SNS messages.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task FunctionHandler(SNSEvent evnt, ILambdaContext context)
+        {
+            // SNS only ever sends one record per event (https://stackoverflow.com/a/47796870)
+            await ProcessRecordAsync(evnt.Records[0], context);
+        }
+
+        private async Task ProcessRecordAsync(SNSEvent.SNSRecord record, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Processed record {record.Sns.Message}");
+
+            // Simulate a DynamoDB request (single-digit ms response)
+            await Task.Delay(6);
+        }
+    }
+}
