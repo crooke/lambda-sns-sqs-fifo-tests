28b9e3354fabdaff4725eb2d02324fac429b7bb6
Eric Crook <eric.crook@gmail.com>
2020-12-17 14:49:17 -0600

    Add benchmark test

--- SnsProcessor/test/SnsProcessor.Tests/FunctionTest.cs
+++ SnsProcessor/test/SnsProcessor.Tests/FunctionTest.cs
@@ -1,18 +1,25 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
 using Xunit;
 using Amazon.Lambda.TestUtilities;
 using Amazon.Lambda.SNSEvents;
-
-using SnsProcessor;
+using Amazon.SimpleNotificationService;
+using System.Linq;
+using System.Diagnostics;
+using Xunit.Abstractions;
+using System;
 
 namespace SnsProcessor.Tests
 {
     public class FunctionTest
     {
+        private readonly ITestOutputHelper output;
+
+        public FunctionTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public async Task TestSQSEventLambdaFunction()
         {
@@ -40,6 +47,25 @@ namespace SnsProcessor.Tests
             await function.FunctionHandler(snsEvent, context);
 
             Assert.Contains("Processed record foobar", logger.Buffer.ToString());
+        }
+
+        [Fact]
+        public async Task BenchmarkTestAsync()
+        {
+            System.Environment.SetEnvironmentVariable("AWS_PROFILE", "dev-enc");
+            var snsClient = new AmazonSimpleNotificationServiceClient();
+            var topicArn = "arn:aws:sns:us-east-1:099157907345:MessageProcessor-SnsTopic-1ACCA72168YXE";
+            var numConcurrentTasks = 5_000;
+
+            output.WriteLine($"Start time: {DateTimeOffset.UtcNow}");
+            var stopwatch = Stopwatch.StartNew();
+
+            var tasks = Enumerable.Range(0, numConcurrentTasks).Select(i => snsClient.PublishAsync(topicArn, $"Message #{i}"));
+            await Task.WhenAll(tasks);
+
+            stopwatch.Stop();
+            output.WriteLine($"End time: {DateTimeOffset.UtcNow}");
+            output.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
